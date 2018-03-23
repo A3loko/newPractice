6d2709d427df9c92bcccbf409e30d596165e7eb6
a3loko <lokesh09rishi@gmail.com>
2018-03-23 17:49:08 +0530

    sdjk

---
+++ src/main/java/com/example/webserver/webserverpractice/WebserverPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.webserver.webserverpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserverPracticeApplication.class, args);
+	}
+}
